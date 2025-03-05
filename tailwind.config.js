1cd0e642566b65741cb557311e0c12a727d1ab46
Amin <aminsahrae4@gmail.com>
2025-03-06 00:55:20 +0330

    /.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./*.{html,js}"
   ],
   theme: {
+    boxShadow: {
+      'ax-hover': '0 0 25px 0 rgba(245, 234, 182, 0.5)',
+      'ax': '0 0 25px 0 rgba(245, 234, 182, 0.5)',
+    },
+    
+    fontFamily: {
+      'amiri': ['Amiri', 'serif'],
+    },
+   
     screens: {
       'sm': '480px',
       'md': '768px',
@@ -12,7 +21,19 @@ module.exports = {
       'xl': '1280px',
       '2xl': '1536px',
     },
-    extend: {},
+    extend: { 
+      colors: {
+        'f5eab6': '#f5eab6',
+        'black': '#000000',
+        'white': '#ffffff', 
+      },
+     
+     
+      
+
+
+      
+    },
   },
   plugins: [],
 } 
