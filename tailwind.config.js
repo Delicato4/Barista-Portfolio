98bdd2226a24fffc0dc53fc9595eb87ee0857119
Amin <aminsahrae4@gmail.com>
2025-03-06 01:30:10 +0330

    Fix CSS paths and merge conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./*.html",
-    "./src/**/*.{html,js}",
-  ],
-  theme: {
-    boxShadow: {
-      'ax-hover': '0 0 25px 0 rgba(245, 234, 182, 0.5)',
-      'ax': '0 0 25px 0 rgba(245, 234, 182, 0.5)',
-    },
-    
-    fontFamily: {
-      'amiri': ['Amiri', 'serif'],
-    },
-   
-    screens: {
-      'sm': '480px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
-    extend: { 
-      colors: {
-        'f5eab6': '#f5eab6',
-        'black': '#000000',
-        'white': '#ffffff', 
-      },
-     
-     
-      
-
-
-      
-    },
-  },
-  plugins: [],
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -76,5 +36,4 @@ module.exports = {
     },
   },
   plugins: [],
->>>>>>> 23047fcc468975b3c147c22ca034a41fdde26e1d
 } 
